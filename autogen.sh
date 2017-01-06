327e2b87d9e45e7976a2c0fff037831ca980bc81
Carlo Wood <carlo@alinoe.com>
2017-01-06 18:39:28 +0100

    Initial project creation, core build system files.

---
+++ autogen.sh
@@ -0,0 +1,55 @@
+#! /bin/sh
+
+if test -f .git; then
+  echo "Error: don't run $0 inside a submodule. Run it from the parent project's directory."
+  exit 1
+fi
+
+if test "$(realpath $0)" != "$(realpath $(pwd)/autogen.sh)"; then
+  echo "Error: run autogen.sh from the directory it resides in."
+  exit 1
+fi
+
+if test -d .git; then
+  # The following line is parsed by configure.ac to find the maintainer hash.
+  MAINTAINER_HASH=15014aea5069544f695943cfe3a5348c
+  # If this was a clone without --recursive, fix that fact.
+  if test ! -e cwm4/scripts/bootstrap.sh; then
+    git submodule update --init --recursive
+  fi
+  # Update autogen.sh and cwm4 itself if we are the real maintainer.
+  if test -f cwm4/scripts/real_maintainer.sh; then
+    cwm4/scripts/real_maintainer.sh $MAINTAINER_HASH
+    RET=$?
+    # A return value of 2 means we need to continue with calling bootstrap.sh.
+    # Otherwise, abort/stop returning that value.
+    if test $RET -ne 2; then
+      exit $RET
+    fi
+  fi
+  # Update all submodules.
+  if ! cwm4/scripts/update_submodules.sh --recursive; then
+    echo "autogen.sh: Failed to update one or more submodules. Does it have uncommitted changes?"
+    exit 1
+  fi
+  # (Re)generate submodules.m4.
+  cwm4/scripts/generate_submodules_m4.sh
+else
+  # Clueless user check.
+  if test -f configure; then
+    echo "You only need to run './autogen.sh' when you checked out this project from the git repository."
+    echo "Just run ./configure [--help]."
+    if test -e cwm4/scripts/bootstrap.sh; then
+      echo "If you insist on running it and know what you are doing, then first remove the 'configure' script."
+    fi
+    exit 0
+  elif test ! -e cwm4/scripts/bootstrap.sh; then
+    echo "Houston, we have a problem: the cwm4 git submodule is missing from your source tree!?"
+    echo "I'd suggest to clone the source code of this project from github:"
+    echo "git clone --recursive https://github.com/CarloWood/ai-threadsafe-testsuite.git"
+    exit 1
+  fi
+fi
+
+# Run the autotool commands.
+exec cwm4/scripts/bootstrap.sh
---
+++ debug.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define NAMESPACE_DEBUG debug
+#define NAMESPACE_DEBUG_START namespace debug {
+#define NAMESPACE_DEBUG_END }
+#include "cwd/debug.h"
