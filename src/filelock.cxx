8e140297b1495670ba35b3281533f3fb7884cf08
Carlo Wood <carlo@alinoe.com>
2017-04-17 16:59:17 +0200

    Add filelock.cxx and microbench.
    Turns out that a lock/unlock pair of
    a boost::interprocess::file_lock takes
    less than a microsecond.

---
+++ src/filelock.cxx
@@ -0,0 +1,36 @@
+#include "sys.h"
+#include "debug.h"
+#include "statefultask/AIAuxiliaryThread.h"
+#include "microbench/microbench.h"
+#include <chrono>
+#include <boost/interprocess/sync/file_lock.hpp>
+
+int main()
+{
+#ifdef DEBUGGLOBAL
+  GlobalObjectManager::main_entered();
+#endif
+  Debug(NAMESPACE_DEBUG::init());
+
+  AIAuxiliaryThread::start();
+
+  boost::interprocess::file_lock flock("my_lock_file");
+  Dout(dc::notice|flush_cf, "Attempting to lock \"my_lock_file\".");
+  flock.lock();
+  Dout(dc::notice|flush_cf, "Lock obtained; sleeping 100 ms...");
+  std::this_thread::sleep_for(std::chrono::milliseconds(100));
+  Dout(dc::notice|flush_cf, "Running benchmark...");
+
+  std::cout << "Average time per lock/unlock pair: " <<
+    moodycamel::microbench(
+        [&flock]() {            // Function to benchmark.
+          flock.unlock();
+          flock.lock(); },
+        100000,                 // Iterations per test run.
+        20,                     // Number of test runs.
+        true) * 1000            // ms -> us
+    << " µs." << std::endl;
+
+  flock.unlock();
+  AIAuxiliaryThread::stop();
+}
