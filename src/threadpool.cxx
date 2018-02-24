be5032ae5de48b04aa62bd3ca25ff0c538b9c0cf
Carlo Wood <carlo@alinoe.com>
2018-02-24 18:05:00 +0100

    WIP

--- src/threadpool.cxx
+++ src/threadpool.cxx
@@ -3,6 +3,8 @@
 #include "debug.h"
 #include <chrono>
 
+int constexpr capacity = 128;
+
 int main()
 {
 #ifdef DEBUGGLOBAL
@@ -13,19 +15,39 @@ int main()
   std::atomic_int count{0};
   {
     AIThreadPool thread_pool;
-    AIThreadPool::QueueHandle queue_handle = thread_pool.new_queue(8);
+    AIThreadPool::QueueHandle queue_handle = thread_pool.new_queue(capacity);
     {
       auto queues_access = thread_pool.queues_read_access();
 
+      size_t delay = 1000000;
       for (int n = 0; n < 1000000; ++n)
       {
-        AIObjectQueue<std::function<void()>> const& queue = thread_pool.get_queue(queues_access, queue_handle);
-        auto access = queue.producer_access();
-        int length = access.length();
-        if (length < 8) // Buffer not full?
-          access.move_in([&count](){ Dout(dc::notice, count << ": hello pool!"); count++; });
+        // Note the reference (&) here (it won't compile without it).
+        auto& queue = thread_pool.get_queue(queues_access, queue_handle);
+        int length;
+        {
+          auto access = queue.producer_access();
+          length = access.length();
+          if (length < capacity) // Buffer not full?
+            access.move_in([&count](){ Dout(dc::notice, count << ": hello pool!"); count++; });
+        }
+        if (AI_UNLIKELY(length == capacity))
+        {
+          Dout(dc::notice, "Queue was full (n = " << n << "; delay = " << delay << ").");
+          delay *= 1.01;
+        }
         else
-          std::this_thread::sleep_for(std::chrono::milliseconds(1));
+          queue.notify_one();
+        if (length <= 1)
+        {
+          delay *= 0.99;
+        }
+        else if (length >= capacity / 2)
+        {
+          size_t count = delay;
+          for (size_t i = 0; i < count; ++i)
+            ;
+        }
       }
     }
 
