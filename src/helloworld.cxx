fd76211061a211f3aca5fa23565e360838ffd7f5
Carlo Wood <carlo@alinoe.com>
2018-06-07 16:53:53 +0200

    Fix helloworld.cxx example; add comments.

--- src/helloworld.cxx
+++ src/helloworld.cxx
@@ -2,7 +2,6 @@
 #include "debug.h"
 #include "statefultask/AIEngine.h"
 #include "statefultask/AIThreadPool.h"
-#include "utils/GlobalObjectManager.h"
 #include <iostream>
 #include <chrono>
 #include <atomic>
@@ -15,7 +14,9 @@ class Bumper;
 HelloWorld* hello_world;
 Bumper* bumper;
 
-class HelloWorld : public AIStatefulTask {
+// Create a task called HelloWorld with three states.
+class HelloWorld : public AIStatefulTask
+{
   private:
     bool m_bumped;
 
@@ -24,24 +25,28 @@ class HelloWorld : public AIStatefulTask {
 
     // The different states of the task.
     enum hello_world_state_type {
-      HelloWorld_start = direct_base_type::max_state,
+      HelloWorld_start = direct_base_type::max_state,                   // Mandatory first value.
       HelloWorld_wait,
       HelloWorld_done,
     };
 
   public:
-    static state_type constexpr max_state = HelloWorld_done + 1;
+    static state_type constexpr max_state = HelloWorld_done + 1;        // Must be one passed the last state.
     HelloWorld();
 
+    // Raise signal '1' when this function is called.
     void bump() { m_bumped = true; signal(1); }
 
   protected: // The destructor must be protected.
+    // Override virtual functions of the base class.
     ~HelloWorld() override;
     char const* state_str_impl(state_type run_state) const override;
     void multiplex_impl(state_type run_state) override;
 };
 
-class Bumper : public AIStatefulTask {
+// Create a task called Bumper with three states.
+class Bumper : public AIStatefulTask
+{
   private:
     bool m_bumped;
 
@@ -50,23 +55,26 @@ class Bumper : public AIStatefulTask {
 
     // The different states of the task.
     enum bumper_state_type {
-      Bumper_start = direct_base_type::max_state,
+      Bumper_start = direct_base_type::max_state,                       // Mandatory first value.
       Bumper_wait,
       Bumper_done,
     };
 
   public:
-    static state_type constexpr max_state = Bumper_done + 1;
+    static state_type constexpr max_state = Bumper_done + 1;            // Must be one passed the last state.
     Bumper();
 
+    // Raise signal '1' when this function is called.
     void bump() { m_bumped = true; signal(1); }
 
   protected: // The destructor must be protected.
+    // Override virtual functions of the base class.
     ~Bumper() override;
     char const* state_str_impl(state_type run_state) const override;
     void multiplex_impl(state_type run_state) override;
 };
 
+// Pass true to the base class to turn on debug output for this task.
 HelloWorld::HelloWorld() : DEBUG_ONLY(AIStatefulTask(true),) m_bumped(false)
 {
   DoutEntering(dc::statefultask, "HelloWorld::HelloWorld()");
@@ -79,6 +87,7 @@ HelloWorld::~HelloWorld()
   --number_of_tasks;
 }
 
+// Allow human readable states.
 char const* HelloWorld::state_str_impl(state_type run_state) const
 {
   switch(run_state)
@@ -92,6 +101,7 @@ char const* HelloWorld::state_str_impl(state_type run_state) const
   return "UNKNOWN STATE";
 };
 
+// Pass true to the base class to turn on debug output for this task.
 Bumper::Bumper() : DEBUG_ONLY(AIStatefulTask(true),) m_bumped(false)
 {
   DoutEntering(dc::statefultask, "Bumper::Bumper()");
@@ -104,6 +114,7 @@ Bumper::~Bumper()
   --number_of_tasks;
 }
 
+// Allow human readable states.
 char const* Bumper::state_str_impl(state_type run_state) const
 {
   switch(run_state)
@@ -117,75 +128,102 @@ char const* Bumper::state_str_impl(state_type run_state) const
   return "UNKNOWN STATE";
 };
 
+// The HelloWorld task waits until it gets 'bumped'.
+// It then bumps bumper and finishes.
 void HelloWorld::multiplex_impl(state_type run_state)
 {
   switch(run_state)
   {
-    case HelloWorld_start:
-      set_state(HelloWorld_wait);
+    case HelloWorld_start:              // First state is the entry point.
+      set_state(HelloWorld_wait);       // Continue in state HelloWorld_wait.
       break;
     case HelloWorld_wait:
       if (!m_bumped)
       {
-        wait(1);
+        wait(1);                        // If not bumped yet, wait for signal '1'.
         break;
       }
-      set_state(HelloWorld_done);
+      set_state(HelloWorld_done);       // If bumped continue with state HelloWorld_done.
       break;
     case HelloWorld_done:
-      bumper->bump();
-      finish();
+      bumper->bump();                   // Bump bumper,
+      finish();                         // and finish.
       break;
   }
 }
 
+// The Bumper task bumps hello_world and then
+// waits until it gets bumped itself, after which
+// it finishes.
 void Bumper::multiplex_impl(state_type run_state)
 {
   switch(run_state)
   {
-    case Bumper_start:
-      hello_world->bump();
-      set_state(Bumper_wait);
+    case Bumper_start:                  // First state is the entry point.
+      hello_world->bump();              // Bump hello_world.
+      set_state(Bumper_wait);           // Continue in the state Bumper_wait.
       break;
     case Bumper_wait:
       if (!m_bumped)
       {
-        wait(1);
+        wait(1);                        // If not bumped yet, wait for signal '1'.
         break;
       }
-      set_state(Bumper_done);
+      set_state(Bumper_done);           // If bumped continue with state Bumper_done.
       break;
     case Bumper_done:
-      finish();
+      finish();                         // Finish.
       break;
   }
 }
 
 int main()
 {
+  // Initialize debugging code. This must be the first line in main().
   Debug(NAMESPACE_DEBUG::init());
   // Make sure the debug channel 'statefultask' is turned on.
   Debug(if (!dc::statefultask.is_on()) dc::statefultask.on());
 
+  // Just a test to make sure that the destructor is protected.
+  // That avoids that one accidently creates a Task on the stack.
+  // Tasks must be created using new.
   static_assert(!std::is_destructible<HelloWorld>::value &&
                 std::has_virtual_destructor<HelloWorld>::value,
-      "Class must have a protected virtual destuctor.");
+      "Class HelloWorld must have a protected virtual destuctor.");
+  static_assert(!std::is_destructible<Bumper>::value &&
+                std::has_virtual_destructor<Bumper>::value,
+      "Class Bumper must have a protected virtual destuctor.");
 
+  // Create an engine. This application isn't really using it,
+  // because everything runs in the thread pool.
+  // Use (any) max_duration (here 10 ms) to cause engine.mainloop()
+  // to return (immediately, because it has nothing to do).
   AIEngine engine("main:engine", 10);
+
+  // Create a thread pool. Immediately afterwards call new_queue on it at least once!
   AIThreadPool thread_pool;
   AIQueueHandle high_priority_queue = thread_pool.new_queue(8);
 
+  // Create our two custom tasks. This uses normal pointers (as opposed
+  // to boost::intrusive_ptr<AIStatefulTask>) which means that they
+  // are destructed immediately after finishing. That can be the case
+  // immediately after returning from run(), so do not use hello_world
+  // or bumper anymore after calling run()!
   hello_world = new HelloWorld;
   bumper = new Bumper;
 
+  // Run the hello_world task in the thread pool through the high_priority_queue.
   Dout(dc::statefultask|flush_cf, "Calling hello_world->run()");
   hello_world->run(high_priority_queue);
   Dout(dc::statefultask|flush_cf, "Returned from hello_world->run()");
 
+  // Run the bumper task in the thread pool through the high_priority_queue.
   Dout(dc::statefultask|flush_cf, "Calling bumper->run()");
   bumper->run(high_priority_queue);
   Dout(dc::statefultask|flush_cf, "Returned from bumper->run()");
 
+  // Wait till both tasks are finished. This loop theoretically
+  // allows use to use engine to also run a task in.
   for (int n = 0; n < 100 && number_of_tasks > 0; ++n)
   {
     Dout(dc::statefultask|flush_cf, "Calling engine.mainloop()");
