827335a50c2cb4469a5531d48fe3caaf8bca397a
Carlo Wood <carlo@alinoe.com>
2018-08-21 20:58:35 +0200

    W.I.P.

--- src/AIResolver_test.cxx
+++ src/AIResolver_test.cxx
@@ -2,6 +2,7 @@
 #include "debug.h"
 #include "resolver-task/AIResolver.h"
 #include "statefultask/AIThreadPool.h"
+#include "resolver-task/dns/src/dns.h"
 
 int constexpr queue_capacity = 32;
 
@@ -12,15 +13,21 @@ int main()
   AIThreadPool thread_pool;
   AIQueueHandle handler __attribute__ ((unused)) = thread_pool.new_queue(queue_capacity);
 
+  // Initialize the IO event loop thread.
+  EventLoopThread::instance().init(handler);
+
   auto handle = AIResolver::instance().request("www.google.com", "www");
   auto handle2 = AIResolver::instance().request("www.google.com", "www");
 
   // Wait till the request is handled.
   while (!handle->is_ready())
-    ;
+    std::this_thread::sleep_for(std::chrono::milliseconds(1));
 
   std::cout << "Result: " << handle->get_result() << std::endl;
   handle.reset();
 
+  // Terminate application.
+  AIResolver::instance().close();
+  EventLoopThread::instance().terminate();
   Dout(dc::notice, "Leaving main()...");
 }
