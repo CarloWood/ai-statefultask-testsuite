7aea52009eae9c80d974f8899e339c116bfb191c
Carlo Wood <carlo@alinoe.com>
2018-09-06 17:47:03 +0200

    WIP on resolver.

--- src/AIResolver_test.cxx
+++ src/AIResolver_test.cxx
@@ -15,27 +15,30 @@ int main()
   // Initialize the IO event loop thread.
   EventLoopThread::instance().init(handler);
   // Initialize the async hostname resolver.
-  Resolver::instance().init(false);
+  Resolver::instance().init(true);
 
-  {
   auto handle2 = Resolver::instance().getaddrinfo("irc.undernet.org", 6668, AddressInfoHints(AI_CANONNAME));
-  }
+  auto handle3 = Resolver::instance().getaddrinfo("www.google.com", "www", AddressInfoHints(AI_CANONNAME));
   auto handle = Resolver::instance().getaddrinfo("irc.undernet.org", "ircd", AddressInfoHints(AI_CANONNAME, AF_UNSPEC, 0 /*, IPPROTO_TCP*/));
 
-  // Wait till the request is handled.
-  while (!handle->is_ready())
+  // Wait till the requests are handled.
+  while (!handle3->is_ready())
     std::this_thread::sleep_for(std::chrono::milliseconds(1));
+  ASSERT(handle->is_ready() && handle2->is_ready());
 
   if (handle->success())
   {
     pthread_mutex_lock(&cout_mutex);
     std::cout << "Result: port = " << handle->get_port() << "; IP#'s = " << handle->get_result() << std::endl;
-    //std::cout << "Result2: port = " << handle2->get_port() << "; IP#'s = " << handle2->get_result() << std::endl;
+    std::cout << "Result2: port = " << handle2->get_port() << "; IP#'s = " << handle2->get_result() << std::endl;
+    std::cout << "Result3: port = " << handle3->get_port() << "; IP#'s = " << handle3->get_result() << std::endl;
     pthread_mutex_unlock(&cout_mutex);
   }
   else
     std::cerr << "Failure: " << handle->get_error() << '.' << std::endl;
   handle.reset();
+  handle2.reset();
+  handle3.reset();
 
   // Terminate application.
   Resolver::instance().close();
