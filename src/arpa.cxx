593e383e2979c426efc71f70946a03dc60f80a40
Carlo Wood <carlo@alinoe.com>
2018-10-21 19:14:35 +0200

    WIP

---
+++ src/arpa.cxx
@@ -0,0 +1,15 @@
+#include "sys.h"
+#include "debug.h"
+#include "evio/SocketAddress.h"
+
+int main(int, char* argv[])
+{
+  Debug(debug::init());
+
+  evio::SocketAddress sa(argv[1]);
+
+  evio::SocketAddress::arpa_buf_t arpa_buf;
+  sa.ptr_qname(arpa_buf);
+
+  Dout(dc::notice, "sa = " << sa << "; ptr qname = \"" << arpa_buf.data() << "\".");
+}
