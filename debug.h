1eb1fdc5b01422c9e1459bd6feba1dac2f1e7132
Carlo Wood <carlo@alinoe.com>
2019-10-28 02:29:17 +0100

    Add support for print_using.

--- debug.h
+++ debug.h
@@ -4,3 +4,9 @@
 #define NAMESPACE_DEBUG_START namespace debug {
 #define NAMESPACE_DEBUG_END }
 #include "cwds/debug.h"
+
+#include "utils/print_using.h"
+namespace libcwd {
+// Allow using PrintUsing inside Dout without the utils:: prefix.
+using utils::print_using;
+} // namespace libcwd
